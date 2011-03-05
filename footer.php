9cecce6ece2835cf51705f9c6f3e22757c86c51d
Rafael Montagud <dranor@dranor.net>
2011-03-05 17:13:58 +0100

    Keep the original author, add github repo with my silly changes

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 	<div id="footer">
-		Theme by <a href="http://www.designer-daily.com">Designer Daily</a> - Powered by <a href="http://www.wordpress.org">Wordpress</a>
+    Theme by <a href="http://www.designer-daily.com">Designer Daily</a> - 
+    With some changes by <a href="http://github.com/rmontagud/happy-cyclope">rmontagud@github</a> -
+    Powered by <a href="http://www.wordpress.org">Wordpress</a>
 	</div>
 </div>
 </div>
