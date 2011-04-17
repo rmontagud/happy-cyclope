64f7d6e9a577fdd8b540464eef02b9ab25d2f6d4
Rafael Montagud <dranor@dranor.net>
2011-04-17 22:34:39 +0200

    dummy commit, source formatting

--- functions.php
+++ functions.php
@@ -1,13 +1,12 @@
 <?php
-
-if ( function_exists('register_sidebar') ):
-	register_sidebar(array(
-		'name' => 'Sidebar',
-		'before_widget' => '', // Removes <li>
-		'after_widget' => '', // Removes </li>
-		'before_title' => '<h2>',
-		'after_title' => '</h2>',
-	));
+if (function_exists('register_sidebar')):
+    register_sidebar(array(
+        'name' => 'Sidebar',
+        'before_widget' => '', // Removes <li>
+        'after_widget' => '', // Removes </li>
+        'before_title' => '<h2>',
+        'after_title' => '</h2>',
+    ));
 endif;
 
 add_theme_support('post-formats', array('link', 'gallery'));
